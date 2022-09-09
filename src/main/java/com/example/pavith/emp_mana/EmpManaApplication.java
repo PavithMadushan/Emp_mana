765752dccfbc8dd0ee4ab2445976eb71fdd278f4
PavithMadushan <pavithmadushan@gmail.com>
2022-09-09 20:43:33 +0530

    Initial commit

---
+++ src/main/java/com/example/pavith/emp_mana/EmpManaApplication.java
@@ -0,0 +1,13 @@
+package com.example.pavith.emp_mana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpManaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmpManaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pavith/emp_mana/EmpManaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pavith.emp_mana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpManaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
